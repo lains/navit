1d85c62cb4f53a550f3bd5528821412a215b9f84
Lionel AINS <lionel@legrand.fr>
2018-02-09 10:00:52 +0100

    Local version of automated android build

--- ci/build_android.sh
+++ ci/build_android.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 set -e
 
-apt-get update && apt-get install -y wget
+#apt-get update && apt-get install -y wget
 
 export ARCH="arm"
 export START_PATH=~/
-export SOURCE_PATH=$START_PATH"/"${CIRCLE_PROJECT_REPONAME}"/"
+export SOURCE_PATH=$START_PATH"/navit-git/"
 export CMAKE_FILE=$SOURCE_PATH"/Toolchain/arm-eabi.cmake"
 export ANDROID_NDK=~/android-ndk-r11c
 export ANDROID_NDK_BIN=$ANDROID_NDK"/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin"
-export ANDROID_SDK="/usr/local/android-sdk-linux/"
+export ANDROID_SDK="/opt/android-sdk-r25.2.5-linux/"
 export ANDROID_SDK_PLATFORM_TOOLS=$ANDROID_SDK"/platform-tools"
 export PATH=$ANDROID_NDK_BIN:$ANDROID_SDK_PLATFORM_TOOLS:$PATH
 export BUILD_PATH=android-${ARCH}
 
-export ANDROID_SDK_HOME=/opt/android-sdk-linux
-export ANDROID_HOME=/opt/android-sdk-linux
+export ANDROID_SDK_HOME=/opt/android-sdk-r25.2.5-linux
+export ANDROID_HOME=/opt/android-sdk-r25.2.5-linux
 export PATH=${PATH}:${ANDROID_SDK_HOME}/tools:${ANDROID_SDK_HOME}/platform-tools:/opt/tools
 
-wget -nv -c http://dl.google.com/android/repository/android-ndk-r11c-linux-x86_64.zip
-[ -d ~/android-ndk-r11c ] || unzip -q -d ~ android-ndk-r11c-linux-x86_64.zip
+#wget -nv -c http://dl.google.com/android/repository/android-ndk-r11c-linux-x86_64.zip
+#[ -d ~/android-ndk-r11c ] || unzip -q -d ~ android-ndk-r11c-linux-x86_64.zip
 
 [ -d $BUILD_PATH ] || mkdir -p $BUILD_PATH
 pushd $BUILD_PATH
